fb70dac9fc7db92101eb5ea96f382e708b35a816
sven5s <Sven.Schiffner@mailbox.tu-dresden.de>
2016-09-20 15:01:04 +0200

    test.c: clean up code base and make output ready for parsing

--- tests/test.c
+++ tests/test.c
@@ -17,6 +17,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.*
  ***********************************************************************/
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <omp.h>
 #include <stdio.h>
@@ -24,151 +25,250 @@
 
 #include "adapt.h"
 
+/* hardcoded path to control the frequency scaling */
 #define SCALING_CUR_FREQ "/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq"
 
-/* Function to print out a file */
+/* Print nice header for a category */
+void
+print_category(int message, char * category)
+{
+    if ( message == 1 )
+        { printf("\n*********\n"); printf("Test %s", category); printf("\n*********\n"); }
+}
+
+/* Make adapt_def_region compact */
 int
-cat(char * filename)
+def_region(int message, char * category, uint64_t bid, int rid)
+{
+    int error;
+    char * prefix = "test_";
+
+    /* string magic to concatenate the string for the region name */
+    char * buffer = calloc(strlen(prefix) + strlen(category) + 1, sizeof(char));
+    strncpy(&buffer[0], prefix, strlen(prefix));
+    strncpy(&buffer[strlen(prefix)], category, strlen(category));
+    buffer[strlen(prefix) + strlen(category) + 1] = '\0';
+
+    if ( message == 1 ) printf("\nDefine region\n");
+    error = adapt_def_region(bid, buffer, rid);
+    if ( message == 1 ) printf("Returned error code: %d\n", error);
+
+    /* free the buffer for the region name */
+    free(buffer);
+
+    return error;
+}
+
+/* Make adapt_enter_stacks compact */
+int
+enter_stacks(int message, uint64_t bid, int rid)
+{
+    int error;
+
+    if ( message == 1 ) printf("\nEnter region with stacks\n");
+    error = adapt_enter_stacks(bid, 0, rid, 0);
+    if ( message == 1 ) printf("Returned error code: %d\n", error);
+
+    return error;
+}
+
+/* Make adapt_exit compact */
+int
+exit_stacks(int message, uint64_t bid, int rid)
+{
+    int error;
+
+    if ( message == 1 ) printf("\nExit region\n");
+    error = adapt_exit(bid, 0, 0);
+    if ( message == 1 ) printf("Returned error code: %d\n", error);
+
+    return error;
+}
+
+/* Print out a file like cat */
+int
+cat(char * filename, int overhead)
 {
     int c;
+    int i = 0;
     FILE *file;
+
+    /* open the given file readonly */
     file = fopen(filename, "r");
 
+    /* we can read the file if it's not a NULL-pointer */
     if (file) {
-        while ((c = getc(file)) != EOF)
+        /* loop over all characters until the EOF */
+        while ((c = fgetc(file)) != EOF)
+        {
+            /* it is possible to skip a specific number of chars */
+            if ( overhead > 0 )
+            {
+                overhead--;
+                continue;
+            }
+            /* for cleaner output we replace every newline with a tab */
+            if ( c == '\n' )
+                c = '\t';
+            /* now we can put the char to stdout */
             putchar(c);
+
+            /* we count how many chars we give back */
+            i++;
+        }
+        /* after EOF we can close the file */
         fclose(file);
     }
     else
-        return 1;
+        /* if something went wront we return an negative number because the file
+         * can be empty and so i would be 0 */
+        return -1;
 
-    return 0;
+    /* give the number of chars back */
+    return i;
 }
 
-/* Function to do print threads for testing */
+/* Print number of threads for testing */
 int
-print_threads(int message)
+print_threads(void)
 {
-    message = 2;
-    if ( message == 1 )
-        printf("Thread %d from %d \n", omp_get_thread_num() + 1, omp_get_num_threads());
-    else
-        printf("Threads: %d \n", omp_get_num_threads());
+    int lock = 0;
+
+    /* lock is shared with all threads */
+    #pragma omp parallel shared(lock)
+    {
+        /* this code is only exexcuted from one thread at the same time */
+        #pragma omp critical
+        {
+            if ( lock != 1 )
+                { printf("%d", omp_get_num_threads()); lock = 1; }
+        }
+    }
 }
 
-/* Function to test dct behavior */
-void
+/* Start with the test environment */
+
+/* threads/dct behavior */
+int
 test_dct(uint64_t bid, int message)
 {
-    int error;
-    uint32_t rid = 42;
-
-    printf("\n"); printf("Define region \n");
-    error = adapt_def_region(bid, "test_dct", rid);
-    printf("Returned error code: %d\n", error);
-
-#pragma omp parallel
-    {
-        // test thread behavior
-        print_threads(message);
-    }
-
-    printf("\n"); printf("Enter region with stacks in Thread: %i \n", omp_get_thread_num());
-    error = adapt_enter_stacks(bid, omp_get_thread_num(), rid, 0);
-    printf("Returned error code: %d\n", error);
-
-#pragma omp parallel
-    {
-        // test thread behavior
-        print_threads(message);
-    }
-
-    printf("\n"); printf("Exit region in Thread %i \n", omp_get_thread_num());
-    error = adapt_exit(bid, omp_get_thread_num(), 0);
-    printf("Returned error code: %d\n", error);
-
-#pragma omp parallel
-    {
-        // test thread behavior
-        print_threads(message);
-    }
-
-}
-
-/* Function to test file behavior */
-void
-test_file(uint64_t bid, int message)
-{
-    int error;
+    char * category = "dct";
+    int error = 0;
     uint32_t rid = 1;
-    char filename[] = "./test_file";
-
-    printf("\n"); printf("Define region \n");
-    error = adapt_def_region(bid, "test_file", rid);
-    printf("Returned error code: %d\n", error);
     
-    printf("\n"); printf("Enter region with stacks \n");
-    error = adapt_enter_stacks(bid, 0, rid, 0);
-    printf("Returned error code: %d\n", error);
+    print_category(message, category);
 
-    printf("\n"); printf("File %s \n", filename);
-    cat(filename);
+    error |= def_region(message, category, bid, rid);
 
-    printf("\n"); printf("Exit region \n");
-    error = adapt_exit(bid, 0, 0);
-    printf("Returned error code: %d\n", error);
-    
+    if ( message == 1 ) printf("Threads:\n");
+    printf("init_dct_threads_before=");
+    print_threads();
+    printf("\n");
 
-    printf("\n"); printf("File %s \n", filename);
-    cat(filename);
+    error |= enter_stacks(message, bid, rid);
 
+    if ( message == 1 ) printf("Threads:\n");
+    printf("bin_dct_threads_before=");
+    print_threads();
+    printf("\n");
+
+    error |= exit_stacks(message, bid, rid);
+
+    if ( message == 1 ) printf("Threads:\n");
+    printf("bin_dct_threads_after=");
+    print_threads();
+    printf("\n");
+
+    if ( message == 1 ) printf("*********\n");
+
+    return error;
 }
 
-/* Function to tes frequency behavior */
-void
+/* frequency/dvfs behavior */
+int
 test_dvfs(uint64_t bid, int message)
 {
-    int error;
+    char * category = "dvfs";
+    int error = 0;
     uint32_t rid = 2;
 
-    printf("\n"); printf("Define region \n");
-    error = adapt_def_region(bid, "test_dvfs", rid);
-    printf("Returned error code: %d\n", error);
+    print_category(message, category);
 
-    printf("\n"); printf("File %s \n", SCALING_CUR_FREQ);
-    cat(SCALING_CUR_FREQ);
+    if ( message == 1 ) printf("\nFile %s \n", SCALING_CUR_FREQ);
+
+    error |= def_region(message, category, bid, rid);
+    
+    if ( message == 1 ) printf("\nFreq:\n");
+    printf("init_dvfs_freq_before=");
+    cat(SCALING_CUR_FREQ, 0);
     printf("\n");
 
-    printf("\n"); printf("Enter region with stacks \n");
-    error = adapt_enter_stacks(bid, 0, rid, 0);
-    printf("Returned error code: %d\n", error);
+    error |= enter_stacks(message, bid, rid);
 
-    printf("\n"); printf("File %s \n", SCALING_CUR_FREQ);
-    cat(SCALING_CUR_FREQ);
+    if ( message == 1 ) printf("\nFreq:\n");
+    printf("bin_dvfs_freq_before=");
+    cat(SCALING_CUR_FREQ, 0);
     printf("\n");
 
-    printf("\n"); printf("Exit region \n");
-    error = adapt_exit(bid, 0, 0);
-    printf("Returned error code: %d\n", error);
+    error |= exit_stacks(message, bid, rid);
 
-    printf("\n"); printf("File %s \n", SCALING_CUR_FREQ);
-    cat(SCALING_CUR_FREQ);
+    if ( message == 1 ) printf("\nFreq:\n");
+    printf("bin_dvfs_freq_after=");
+    cat(SCALING_CUR_FREQ, 0);
     printf("\n");
+
+    if ( message == 1 ) printf("*********\n");
+
+    return error;
+}
+
+/* file behavior */
+int
+test_file(uint64_t bid, int message, char * filename)
+{
+    char * category = "file";
+    int error = 0, overhead = 0;
+    uint32_t rid = 4;
+
+    print_category(message, category);
+
+    error |= def_region(message, category, bid, rid);
+
+    error |= enter_stacks(message, bid, rid);
+
+    if ( message == 1 ) printf("\nFile %s:\n", filename);
+    printf("bin_file_before_clear=");
+    overhead = cat(filename, 0);
+    printf("\n");
+
+    if ( overhead == -1 )
+        fprintf(stderr, "Error: %s not readable\n", filename);
+    
+    error |= exit_stacks(message, bid, rid);
+
+    if ( message == 1 ) printf("\nFile %s:\n", filename);
+    printf("bin_file_after_clear=");
+    cat(filename, overhead);
+    printf("\n");
+
+    if ( message == 1 ) printf("*********\n");
+
+    return error;
 }
 
 
+/*Main funtion to parse command line arguments and execute specific tests */
 int
 main(int argc, char **argv)
 {
+    /* standard filename for the test file to read */
+    char * filename = "./test_file";
+
     /* const variables for better code */
-    static const int verbose = 1;
-    static const int quiet = 2;
-    static const int machine = 4;
+    static const int verbose = 1, machine = 2;
     int message = verbose;
 
-    static int dct = 1;
-    static int dvfs = 2;
-    static int file = 4;
+    static int dct = 1, dvfs = 2, file = 4;
     int test = 0;
 
     int error = 0;
@@ -178,18 +278,16 @@ main(int argc, char **argv)
     {
         if ( strcmp(*argv, "help") == 0 )
         {
-            printf("Use verbose, quiet, machine or simply nothing as parameter");
+            printf("Use verbose, machine or simply nothing as parameter");
             printf("You can also add the unit that should be tested, like) == 0 )");
             printf("%s message dct", argv[0]);
             printf("Possibile values are) == 0 ) all, dct, dvfs and file");
             return 0;
         }
-        else if ( strcmp(*argv, "machine") == 0 )
-            message = machine;
         else if ( strcmp(*argv, "verbose") == 0 )
             message = verbose;
-        else if ( strcmp(*argv, "quiet") == 0 )
-            message = quiet;
+        else if ( strcmp(*argv, "machine") == 0 )
+            message = machine;
         else if ( strcmp(*argv, "dct") == 0 )
             test += dct;
         else if ( strcmp(*argv, "dvfs") == 0 )
@@ -198,47 +296,33 @@ main(int argc, char **argv)
             test += file;
         else if ( strcmp(*argv, "all") == 0 )
             test = dct + dvfs + file;
+        else if ( strstr(*argv, "/") != NULL )
+            filename = *argv;
     }
-    /* if atfer all this the test scenario wasn't set we will set ist */
+    /* if atfer all this the test scenario wasn't set we will set it */
     if ( test == 0 )
         test = dct + dvfs + file;
 
-    /* open adapt and init anything */
-    printf("Open adapter \n");
-    error = adapt_open();
-    printf("Returned error code: %d\n", error);
+    /* open adapt and init everything */
+    if (message == 1) printf("\nOpen adapter \n");
+    error |= adapt_open();
+    if (message == 1) printf("Returned error code: %d\n", error);
 
     /* get binary id */
-    if ( message == 1 )
-        printf("\n"); printf("Add binary \n");
+    if ( message == 1 ) printf("\nAdd binary \n");
     uint64_t bid = adapt_add_binary("test");
-
-    /* needed to be able to set the number of threads */
-    omp_set_dynamic(1);
 
     /* test for file handling */
     if ( test - file >= 0 )
     {
-        if ( message == 1 )
-        {
-            printf("\n"); printf("*********\n"); printf("Test file \n"); printf("*********\n");
-        }
-        test_file(bid, message);
-        if ( message == 1 )
-            printf("*********\n");
+        error |= test_file(bid, message, filename);
         test -= file;
     }
 
     /* test for dvfs or frequency scaling */
     if ( test - dvfs >= 0)
     {
-        if ( message == 1 )
-        {
-            printf("\n"); printf("*********\n"); printf("Test dvfs \n"); printf("*********\n");
-        }
-        test_dvfs(bid, message);
-        if ( message == 1 )
-            printf("*********\n");
+        error |= test_dvfs(bid, message);
         test -= dvfs;
         dvfs = 0;
     }
@@ -246,13 +330,7 @@ main(int argc, char **argv)
     /* test for dct or thread handling */
     if ( test - dct >= 0 )
     {
-        if ( message == 1 )
-        {
-            printf("\n"); printf("*********\n"); printf("Test dct \n"); printf("*********\n");
-        }
-        test_dct(bid, message);
-        if ( message == 0   )
-            printf("*********\n");
+        error |= test_dct(bid, message);
         test -= dct;
         dct = 0;
     }
@@ -260,35 +338,32 @@ main(int argc, char **argv)
     /* test should be zero after all of these tests otherwise something went
      * wrong */
     if ( test != 0 )
-        error = 2;
+        error |= 2;
 
     /* after our work we need to clean up */
-    if ( message == 1 )
-        printf("\n"); printf("Close adapter \n");
+    if ( message == 1 ) printf("\nClose adapter\n");
     adapt_close();
 
-    if ( message == 1 )
-    {
-        printf("\n"); printf("*********\n"); printf("Test after closing \n"); printf("*********\n");
-    }
+    /* and we should control if everything is set correctly after close */
+    print_category(message, "after closing");
 
     if ( dvfs == 0  )
     {
-        printf("dvfs");
-        printf("\n"); printf("File %s \n", SCALING_CUR_FREQ);
-        cat(SCALING_CUR_FREQ);
-        printf("\n\n");
+        if ( message == 1 ) { printf("Test dvfs\n"); printf("Freq:\n"); }
+        printf("init_dvfs_freq_after=");
+        cat(SCALING_CUR_FREQ, 0);
+        printf("\n");
+        if ( message == 1 ) printf("\n");
     }
 
     if ( dct == 0 )
     {
-        printf("dct \n");
-#pragma omp parallel
-        {
-            // test thread behavior
-            print_threads(message);
-        }
+        if ( message == 1 ) { printf("Test dct\n"); printf("Threads:\n"); }
+        printf("init_dct_threads_after=");
+        print_threads();
+        printf("\n");
     }
+
     /* give back an good error code */
     return error;
 }
