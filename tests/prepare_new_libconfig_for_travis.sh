41dee224f28471cd533ae483964bf311faa62be8
sven5s <Sven.Schiffner@mailbox.tu-dresden.de>
2016-09-20 15:08:56 +0200

    prepare_new_libconfig_for_travis.sh: add messages and correct exit status

--- tests/prepare_new_libconfig_for_travis.sh
+++ tests/prepare_new_libconfig_for_travis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# needed for pinning because we don't need all packages from xenial
+echo "Create /etc/apt/preferences"
 cat << EOF | sudo tee /etc/apt/preferences > /dev/null
 Package: libconfig*
 Pin: release n=xenial
@@ -16,6 +18,8 @@ Pin: release o=Ubuntu
 Pin-Priority: -10
 EOF
 
+# create source.list for xenial
+echo "Create /etc/apt/sources.list.d/xenial.list"
 cat << EOF | sudo tee /etc/apt/sources.list.d/xenial.list > /dev/null
 deb http://de.archive.ubuntu.com/ubuntu xenial main restricted universe multiverse
 #deb-src http://de.archive.ubuntu.com/ubuntu xenial main restricted universe multiverse
@@ -29,3 +33,6 @@ deb http://de.archive.ubuntu.com/ubuntu xenial-security main restricted universe
 deb http://de.archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse
 #deb-src http://de.archive.ubuntu.com/ubuntu xenial-backports main restricted universe multiverse
 EOF
+
+# give a good exit status back
+[ -f /etc/apt/sources.list.d/xenial.list -a -f /etc/apt/preferences ]; exit $?
